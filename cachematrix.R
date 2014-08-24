7fdc3aa541127eeebf7e535d4fed33e73880ac99
waivek <waivek@gmail.com>
2014-08-24 23:40:51 +0530

    cacheMatrix added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Tries to emulate OOP in R
 
-## Write a short comment describing this function
+## Creates a matrix with 4 functions to get and set matrix and inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <<- NULL
+    setMatrix <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    getMatrix <- function() x
+    setInverse <- function(inverse) i <<- inverse
+    getInverse <- function() i 
+    list(setMatrix = setMatrix, 
+         getMatrix = getMatrix,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Returns cached inverse if available, else calculates and returns inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if(!is.null(i)) {
+        print("getting cached inverse")
+        return(i)
+    }
+    mat <- x$getMatrix()
+    i <- solve(mat)
+    x$setInverse(i)
+    i
 }
